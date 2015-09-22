28dd1f91326c3315189cd0fc17398bb60cc191aa
Prachetas Prabhu <prachetas.github@gmail.com>
2015-09-22 14:14:09 -0400

    Basic setup with updated figwheel and cider cljs repl support

--- project.clj
+++ project.clj
@@ -5,19 +5,27 @@
   :license {:name "Creative Commons Zero"
             :url "http://creativecommons.org/publicdomain/zero/1.0/"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "0.0-3211"]
+                 [org.clojure/clojurescript "1.7.122"]
                  [org.omcljs/om "0.8.8"]]
   :plugins [[lein-cljsbuild "1.0.5"]
-            [lein-figwheel "0.3.1"]]
-  :figwheel {:nrepl-port 7888}
+            [lein-figwheel "0.4.0"]]
+  :figwheel {:nrepl-port 7888
+             :nrepl-middleware ["cider.nrepl/cider-middleware"
+                                "refactor-nrepl.middleware/wrap-refactor"
+                                "cemerick.piggieback/wrap-cljs-repl"]}
   :target-path "target/%s"
   :clean-targets ^{:protect false} [:target-path "out"]
   :cljsbuild {:builds [{:id "dev"
                         :source-paths ["src"]
                         :figwheel true
-                        :compiler {:main lab-notebook.core}}
+                        :compiler {:main lab-notebook.core}
+                        :optimizations nil}
                        {:id "prod"
                         :source-paths ["src"]
                         :compiler {:optimizations :advanced
                                    :output-to "target/js"}
-                        :externs ["marked.min.js"]}]})
+                        :externs ["marked.min.js"]}]}
+  :profiles {:dev {:plugins [[cider/cider-nrepl "0.9.1"]]
+                   :dependencies [[com.cemerick/piggieback "0.2.1"]
+                                  [org.clojure/tools.nrepl "0.2.10"]]
+                   :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}}})
--- src/lab_notebook/core.cljs
+++ src/lab_notebook/core.cljs
@@ -1,1 +1,18 @@
-(ns lab-notebook.core)
+(ns lab-notebook.core
+  (:require [om.core :as om]
+            [om.dom :as dom]))
+
+(defonce appstate (atom {}))
+
+(defn app-container [cursor component]
+  (reify
+    om/IRender
+    (render [this]
+      (dom/div nil (:msage cursor)
+               (dom/h1 nil "JLLO")
+               (dom/h1 nil "nedlo")
+               (dom/h1 nil "wdzzup")
+               (dom/h1 nil "yoyo")))))
+
+(om/root app-container appstate
+  {:target (. js/document (getElementById "app"))})
